3118f77455afec925a2a99231e69386d258da550
VOFKA . <Yalta-jely@mail.ru>
2024-05-18 04:36:57 +0300

    изменён цвет фона инструкции

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -131,6 +131,7 @@
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = Color.White;
             label1.Location = new Point(149, 266);
             label1.Name = "label1";
             label1.Size = new Size(310, 125);
